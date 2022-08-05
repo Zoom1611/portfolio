375aa5cce98727a8272f860398a059dbee5dfde1
Ivan Razum <ivanrazum1611@gmail.com>
2022-08-05 14:42:26 +0200

    New components and content on page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,12 +8,12 @@ import CSS from "../assets/css.png";
 
 const Skills = () => {
 	return (
-		<div name="skills" className="bg-[#0a192f] text-gray-300">
+		<div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
 			{/* Container */}
 			<div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
 				<div>
 					<p className="text-4xl font-bold inline border-b-4 border-pink-600">
-						Experience
+						Skills
 					</p>
 					<p className="py-4">These are the technologies I've worked with</p>
 				</div>

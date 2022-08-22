603097cfaa759a5044cd68f8f83579cf203a42d2
Ivan Razum <ivanrazum1611@gmail.com>
2022-08-22 19:59:37 +0200

    Changes in navbar component, logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
 	return (
 		<div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
 			<div>
-				<img src={Logo} alt="Logo" style={{ width: "50px" }} />
+				<b className="text-pink-600 text-2xl">IRazum</b>
 			</div>
 
 			{/* Menu */}
